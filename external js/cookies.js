a573945bee0d2fee011edbebabbac8120c7251e3
= <=>
2025-01-06 20:22:08 +0200

    added two new functions to cookies.js

--- external js/cookies.js
+++ external js/cookies.js
@@ -46,5 +46,33 @@ function delete_cookie(key) {
   document.cookie = `${key}=1;expires=${d};`;
 }
 
+function get_cookie_object(key){
 
-export { get_cookie, get_all_cookie, add_cookie, update_cookie, delete_cookie };
+  /**
+   * this function is the same as get_cookie but it uses JSON.parse()
+   * at the end to retrieve the data in original format rather than
+   * a string
+   */
+  let strs = document.cookie.split(";");
+  for (let str of strs) {
+    let key_value = str.split("=");
+    if (key.trim() == key_value[0].trim()) {
+      return JSON.parse(key_value[1]);
+    }
+    else{
+      return undefined
+    }
+  }
+}
+
+function add_cookie_object(key, value){
+  /**
+   * this function is the same as add_get_cookie but it uses JSON.stringify()
+   * on the value to set an array like this "[3, 2, 5]" rather that 3, 2, 5 if we
+   * used the ordinay add_cookie() 
+   * */
+      document.cookie = `${key}=${JSON.stringify(value)};`;
+}
+
+
+export { get_cookie, get_all_cookie, add_cookie, update_cookie, delete_cookie, add_cookie_object, get_cookie_object };
